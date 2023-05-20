3078af1471e1e69cf3962d997e424df77041d3ce
Arioo Barzan <arioobarzan@hotmail.com>
2023-05-20 11:22:58 +0330

    main

---
+++ First_Projects/First_Projects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace First_Projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("plz enter a num:");
+            string number = Console.ReadLine();
+            x = int.Parse(number);
+
+            x *= 3;
+
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
